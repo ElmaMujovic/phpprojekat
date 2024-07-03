5646583fe15efab9e12dd2644af7f01d9e0eb2eb
ElmaMujovic <elma.mujov1c1@gmail.com>
2024-07-03 21:12:07 +0200

    poslednje

--- resources/views/naseadmin.blade.php
+++ resources/views/naseadmin.blade.php
@@ -1,147 +1,109 @@
 @extends('front.layouts.layoutadmin')
-<style>
-        .posebana {
-            font-size: 12px;
-            color: white;
-            font-weight: bold;
-            text-decoration: none;
-        }
-        body, .content, section, .mySlides, .container-vertikalni-divovi {
-    background-color: #f39c12 !important;
-  
-}
 
-        /* Stil za vertikalne divove */
-        .vertikalni-div {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            margin-top: 30px; /* Povećan razmak između vertikalnih divova */
-            background-color: #c3f2fc;
-            color: white;
-            padding: 40px; /* Povećan padding */
-            border-radius: 30px;
-            width: 450px; /* Povećana širina vertikalnih divova */
-            margin-right: 30px; /* Dodatni razmak između divova */
-            position: relative; /* Dodata pozicija relativna za oblik na dnu */
-            margin-bottom:10px;
-        }
-        .slika-vertikalni-div {
-            width: 127%;
-            height: 300px; /* Automatska visina kako bi se očuvali proporcije */
-            border-radius: 30px;
-            margin-top: -40px;
-            mask-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 90"><path fill-rule="evenodd" d="M0,0 H388 V70 Q300,90 258,70 Q258,90 222,70 Q222,90 186,70 Q186,90 150,70 Q150,90 114,70 Q114,90 78,70 Q78,90 42,70 Q42,90 0,70 Z"></path></svg>');
-            mask-size: cover;
-        }
-        
-        .vertikalni-div h3 {
-            font-size: 22px;
-    line-height: 28px;
-    font-weight: bold;
-    margin: 0;
-    color: #02b7fc;
-    text-align: center;
-    position: relative;
-}
-
-
-        .vertikalni-div p {
-        font-size: 18px;
-    font-weight: 400;
-    line-height: 24px;
-    margin: 1em 0;
-        }
-
-        .vertikalni-div .procitaj-vise {
-            position: relative;
-    overflow: hidden;
-    outline: none;
-    text-decoration: none;
-    display: inline-flex;
-    padding: 4px;
-    cursor: pointer;
-    background-color: #fa84da;
-    border-radius: 8px;
-    margin: 1em auto 0;
-
-    color: #000000;
-    padding: 4px 20px;
-    border: 2px dashed #fff;
-    border-radius: 8px;
-    font-size: 20px;
-    font-weight: 500;
-        }
-
-        .vertikalni-div .procitaj-vise:hover {
-            background-color: #2980b9;
-            color: white;
-        }
-
-        h4{
-    content: "";
-    margin: 0;
-    width: 100%;
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    border: 2px dashed #03b9fa;
-}
-h3{
-    
-}
-
-        /* Stil za container vertikalnih divova */
-        .container-vertikalni-divovi {
-            display: flex;
-            justify-content: center; 
-            margin-top: 50px; 
-            background-color: orange;
-            margin-bottom:50px;
-
-        }
-
-        @media (max-width: 768px) {
-    .vertikalni-div {
-        width: 100%;
-        margin-right: 0;
-    }
-
-    .container-vertikalni-divovi {
-        flex-direction: column;
-        align-items: center;
-    }
-}
-
-    </style>
 @section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dobrodošli</title>
+    <link rel="stylesheet" href="{{ asset('css/pocetna.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+
 <section>
     <div class="mySlides">
         <img src="{{ asset('images/pocetn1.jpg') }}" style="width: 100%" />
     </div>
 </section>
 
+<div class="tekst">
+    <p class="poruka">Dobrodošli na mesto gde se svako dete razvija, uči i raste kroz bezbedno, podržavajuće i kreativno okruženje.</p>
+</div>
 
-<div class="container-vertikalni-divovi">
-    <div class="vertikalni-div">
-        <img src="{{ asset('images/teacher.jpg') }}" alt="Opis slike 1" class="slika-vertikalni-div">
-        <h3>Poučne aktivnosti za vaspitače</h3>
-        <p>Vaspitači mogu pronaći korisne resurse koji olakšavaju njihov rad u učenju i razvoju dece. Sa jasnim smernicama za implementaciju učenja</p>
-        <!-- <a href="{{ route('nase-srecno-mesto') }}" class="procitaj-vise">Pročitaj više</a> -->
+<div class="ikonice-container">
+    <div class="ikonica">
+        <i class="fas fa-gem ikon"></i>
+        <h3>Profesionalni vaspitači</h3>
+        <p>Naš tim čine iskusni profesionalci sa relevantnim obrazovanjem i stručnim kvalifikacijama.</p>
     </div>
-
-    <div class="vertikalni-div">
-        <img src="{{ asset('images/parent.png') }}" alt="Opis slike 2" class="slika-vertikalni-div">
-        <h3>Resursi za roditelje</h3>
-        <p>Roditelji mogu računati na obimnu kolekciju resursa koji podržavaju njihovu ulogu u vaspitanju i edukaciji svoje dece. Saveti stručnjaka, relevantni</p>
-        <!-- <a href="{{ route('nase-srecno-mesto') }}" class="procitaj-vise">Pročitaj više</a> -->
+    <div class="ikonica">
+        <i class="fas fa-star ikon"></i>
+        <h3>Naš pristup</h3>
+        <p>Posvećeni smo pružanju sveobuhvatnog i individualizovanog pristupa razvoju svakog deteta.</p>
     </div>
-
-    <div class="vertikalni-div">
-        <img src="{{ asset('images/kindergarten.jpg') }}" alt="Opis slike 3" class="slika-vertikalni-div">
-        <h3>Zabava kroz učenje za decu</h3>
-        <p>Naš sajt pruža deci predškolskog uzrasta nezaboravno iskustvo učenja kroz igru. Interaktivne igre, šarene animacije i maštovite priče podstiču radoznalost</p>
-        <!-- <a href="{{ route('nase-srecno-mesto') }}" class="procitaj-vise">Pročitaj više</a> -->
+    <div class="ikonica">
+        <i class="fas fa-heart ikon"></i>
+        <h3>Edukativni program</h3>
+        <p>Posvećeni smo pružanju izuzetnog edukativnog iskustva koje inspiriše, motiviše i podstiče rast vaše dece.</p>
     </div>
 </div>
+
+<div class="article-section">
+    <div class="naslov-div">
+        <h3><a href="{{ route('sta_je_stem') }}">Šta je to STEM?</a></h3>
+        <p><a class="last-div" href="#">Pogledaj video</a></p>
+    </div>
+
+    <div class="naslov-div">
+        <h3><a href="{{ route('zasto_stem') }}">Zašto STEM?</a></h3>
+        <p><a class="last-div" href="#">Pogledaj video</a></p>
+    </div>
+
+    <div class="naslov-div">
+        <h3><a href="{{ route('kako_stem') }}">Kako STEM?</a></h3>
+        <p><a class="last-div" href="#">Pogledaj video</a></p>
+    </div>
+
+    <div class="naslov-div">
+        <h3><a href="{{ route('vezbe_i_aktivnosti') }}">Vežbe i aktivnosti</a></h3>
+        <p><a class="last-div" href="#">Pogledaj video</a></p>
+    </div>
+</div>
+
+<section class="blog_part section_padding" >
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-2">
+                    <div class="section_tittle text-center">
+                        <h2 class="h22">NOVOSTI</h2>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="row2">
+    @foreach ($news as $new)
+    <div class="col-sm-4 col-lg-4 col-xl-4">
+        <div class="single-home-blog">
+            <div class="card">
+                @if($new->image)
+                <img src="{{ asset('storage/' . $new->image) }}" class="card-img-top" alt="blog">
+                @else
+                <img src="{{ asset('front/img/loginsl.jpg') }}" class="card-img-top" style="height: 200px; object-fit: cover;" alt="blog">
+                @endif
+                <div class="card-body">
+                    <a href="/login">
+                        <h5 class="card-title">{{ $new->name }}</h5>
+                    </a>
+                    <p>{{ $new->detail }}</p>
+                    <ul>
+                        <li>datum: {{ $new->created_at }}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endforeach
+</div>
+
+
+    </section>
+
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/main.js') }}"></script>
+
+</body>
+</html>
+
 @endsection
